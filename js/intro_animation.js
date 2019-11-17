f822e03ab6fb4f4a04e4711546b481880933bce2
tudor96 <gltudor96@gmail.com>
2019-11-17 15:21:24 +0200

    section 2 implementation, seo tags

--- js/intro_animation.js
+++ js/intro_animation.js
@@ -1,18 +1,9 @@
-$(document).ready(function()
-{   
+$(document).ready(function() {
     anime({
         targets: "#header-image",
-        duration:0,
-        translateY: 100,
-      });
-    anime({
-        targets: "#header-image",
-        opacity:1,
-        duration:400,
-        translateY: 0,
-        delay:200,
-        easing: 'easeInOutQuad'
-      });
-
-
+        translateY: -20,
+        direction: "alternate",
+        loop: true,
+        easing: "easeInOutSine"
+    });
 });
