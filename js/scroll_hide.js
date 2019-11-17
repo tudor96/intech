5a37af4573ee04d6f59bbe6f865598ddcdc2b6bc
tudor96 <gltudor96@gmail.com>
2019-11-17 15:25:50 +0200

    script refactor

--- js/scroll_hide.js
+++ js/scroll_hide.js
@@ -1,42 +1,27 @@
-$(document).ready(function()
-{   
-    console.log("script is working",  $("#page-wrapper"));
-    var animation_trigger =0;
-
-    $(window).scroll(function()
-    {
-        console.log("trigger?!");
-        var nav_bar = $(".navbar-custom"); 
+$(document).ready(function() {
+    var animation_trigger = 0;
+    $(window).scroll(function() {
         var div = $(this);
-        console.log("THIS DIV::"+ div + div.scrollTop());
-        
-        if (div.scrollTop() == 0)
-        {
-            if(animation_trigger){
-                console.log("trigger?!", div.scrollTop() + "wut");
+        if (div.scrollTop() == 0) {
+            if (animation_trigger) {
                 anime({
                     targets: ".navbar-custom",
-                    opacity:0,
-                    duration:100,
-                    easing: 'easeInOutQuad'
-                  });
-                  animation_trigger =0;
+                    opacity: 0,
+                    duration: 100,
+                    easing: "easeInOutQuad"
+                });
+                animation_trigger = 0;
             }
-            
-        }
-        else 
-        {
-            if(!animation_trigger){
-                console.log("fired animation", nav_bar)
+        } else {
+            if (!animation_trigger) {
                 anime({
                     targets: ".navbar-custom",
-                    opacity:1,
-                    duration:200,
-                    easing: 'easeInOutQuad'
-                  });
-                  animation_trigger =1;
+                    opacity: 1,
+                    duration: 200,
+                    easing: "easeInOutQuad"
+                });
+                animation_trigger = 1;
             }
-            // alert("Reached the top!");
         }
     });
 });
